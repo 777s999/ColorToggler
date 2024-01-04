7635a0c89583a82b495003c1c52973ed26535437
Subrata Rout <subrataa916@gmail.com>
2024-01-04 22:43:35 +0530

    adding color toggler project

---
+++ script.js
@@ -0,0 +1,8 @@
+'use script';
+let toggler = document.getElementById('switch');
+
+toggler.addEventListener('click', () => {
+  toggler.checked === true
+    ? (document.body.style.backgroundColor = 'black')
+    : (document.body.style.backgroundColor = 'white');
+});
